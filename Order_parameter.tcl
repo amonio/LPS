7e847c979c2be682ae7c9f35e416f7aaf06411b7
amonio <41928417+amonio@users.noreply.github.com>
2018-08-02 11:17:52 -0400

    update 2
    now, the data is listed and then pulled out

--- Order_parameter.tcl
+++ Order_parameter.tcl
@@ -1,12 +1,14 @@
-proc Scc {archivo frames} {
+proc Scc {frames} {
+set lista []
 for {set tail 1} {$tail < 9} {if {$tail == 1 || $tail == 3} {incr tail 2} else {incr tail}} {
+puts "measuring tail $tail"
 if {$tail != 6} {
     set cmax_num 14
   } else {
     set cmax_num 13
   }
 for {set frag 0} {$frag < 72} {incr frag} {
-for {set frame 0} {$frame < $n} {incr frame} {
+for {set frame 0} {$frame < $frames} {incr frame} {
 set order_list {}
 for {set c_num 2} {$c_num < $cmax_num} {incr c_num} {
     set CXN_1 [atomselect top "fragment $frag and resname ECLI and name C$tail$c_num"]
@@ -17,21 +19,25 @@ for {set c_num 2} {$c_num < $cmax_num} {incr c_num} {
     set c1_coords [lindex [$CXN_1 get {x y z}] 0]
     set c0_coords [lindex [$CXN_0 get {x y z}] 0]
     set cos_0 [lindex [vecnorm [vecsub $c1_coords $c0_coords]] 2]
-    set Scc [expr 1.5*$cos_0*$cos_0 - 0.5]
+    set Scc [expr 1.5 * $cos_0 * $cos_0 - 0.5]
     lappend order_list $Scc
     $CXN_1 delete
     $CXN_0 delete
 }
-puts $archivo "$frame\t$tail\t$frag\t[join $order_list "\t"]" # Change this to accelerate the output. (lappend).
+lappend lista "$frame\t$tail\t$frag\t[join $order_list "\t"]"
 }    
 }
 }
-}
-}
-puts "calculando parametros de orden"
-set n [molinfo top get numframes]
 set open_file [open "raw_data.dat" w]
 puts $open_file "frame\ttail\tfragment\tC2\tC3\tC4\tC5\tC6\tC7\tC8\tC9\tC10\tC11\tC12\tC13\tC14"
-Scc ${open_file} $n
+foreach k $lista {
+	puts $open_file $k
+}
 close $open_file
+}
+
+puts "calculando parametros de orden"
+
+set n [molinfo top get numframes]
+Scc $n
 puts finalizado
